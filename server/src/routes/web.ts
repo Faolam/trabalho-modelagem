b4f31a0b7a44d5b585c8a6882275e0bb88406ffb
victordiogouf <victor.diogo@estudante.ufjf.br>
2023-12-06 15:29:09 -0300

    pimba servidor

--- server/src/routes/web.ts
+++ server/src/routes/web.ts
@@ -6,30 +6,27 @@ import { Produto } from "../Entities/Produto";
 const web = express.Router();
 const { api } = serverConfigs;
 
-web.get(`${api}/web/listProducts`, async (req, res) => 
-  {
-    const produtos = await Produto.listAllProducts();
+web.get(`${api}/web/listProducts`, async (req, res) => {
+  const produtos = await Produto.listAllProducts();
 
-    return res.json({ status: 200, auth: true, data: produtos}).end();
-  }
+  return res.json({ status: 200, auth: true, data: produtos }).end();
+}
 );
 
-web.get(`${api}/web/listRatings`, async (req, res) => 
-  {
-    const ratings = await Produto.listAllRatings();
+web.get(`${api}/web/listRatings`, async (req, res) => {
+  const ratings = await Produto.listAllRatings();
 
-    return res.json({ status: 200, auth: true, data: ratings}).end();
-  }
+  return res.json({ status: 200, auth: true, data: ratings }).end();
+}
 );
 
-web.get(`${api}/web/findProduct`, async (req, res) => 
-  {
-    if (!req.body.name) return res.json({ status: 432, auth: false, data: null }).end();
+web.get(`${api}/web/findProduct`, async (req, res) => {
+  if (!req.query.name) return res.json({ status: 432, auth: false, data: null }).end();
 
-    const findedProduct = await Produto.findProductByName(req.body.name.toString());
+  const findedProduct = await Produto.findProductByName(req.query.name.toString());
 
-    return res.json({ status: 200, auth: true, data: findedProduct}).end();
-  }
+  return res.json({ status: 200, auth: true, data: findedProduct }).end();
+}
 );
 
 
