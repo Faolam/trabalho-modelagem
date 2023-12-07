bb3853cd8257c9720890e1e486e4ab4f2f40c7af
victordiogouf <victor.diogo@estudante.ufjf.br>
2023-12-07 13:00:13 -0300

    acabou?

--- web/src/app/admin/cadastro/page.js
+++ web/src/app/admin/cadastro/page.js
@@ -5,15 +5,17 @@ import Link from 'next/link';
 import { AuthContext } from '@/contexts/auth';
 import style from "./page.module.css";
 import { useRouter } from 'next/navigation';
+import { server } from '@/server';
 
 
 export default function CadastroAdmin() {
+  const { user, token } = useContext(AuthContext);
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
+  const [telefone, setTelefone] = useState('');
   const [repeteSenha, setRepeteSenha] = useState('');
   const router = useRouter();
-  const { user } = useContext(AuthContext);
 
   useEffect(() => {
     if (!user || user && user.permissionLevel != 1) {
@@ -26,9 +28,37 @@ export default function CadastroAdmin() {
     return <></>;
   }
 
-  function enviar() {
-    let texto = "Nome: " + nome + "\nEmail: " + email + "\nSenha: " + senha + "\nSenha Repetida: " + repeteSenha
-    alert(texto)
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (senha !== repeteSenha) {
+      alert("As senhas não coincidem");
+      return;
+    }
+
+    server.post('/admin/newUser', {
+      name: nome,
+      email: email,
+      password: senha,
+      phone: telefone,
+
+    }, { headers: { authorization: token } })
+
+      .then(response => {
+        if (response.data.status !== 200) {
+          throw new Error();
+        }
+        setNome('');
+        setEmail('');
+        setSenha('');
+        setTelefone('');
+        setRepeteSenha('');
+        alert("Administador Cadastrado!");
+      })
+      .catch(error => {
+        console.log(error);
+        alert("Não foi possível realizar o cadastro. Tente novamente mais tarde.")
+      });
   }
 
   return (
@@ -39,11 +69,14 @@ export default function CadastroAdmin() {
           <h1 className={style.titulo}>Cadastro de Administrador</h1>
         </header>
         <forms className={style.form}>
-          <input placeholder="Nome Completo" type='text' id='campo_nome' onChange={(e) => setNome(e.target.value)} /><br />
-          <input placeholder="Email" type='text' id='campo_email' onChange={(e) => setEmail(e.target.value)} /><br />
-          <input placeholder="Senha" type='password' id='campo_senha' onChange={(e) => setSenha(e.target.value)} /><br />
-          <input placeholder="Repetir a Senha" type='password' id='campo_repete_senha' onChange={(e) => setRepeteSenha(e.target.value)} /><br />
-          <input value="Cadastrar" type='submit' onClick={(e) => enviar()} /><br />
+          <br />
+          <br />
+          <input placeholder="Nome Completo" type='text' id='campo_nome' value={nome} onChange={(e) => setNome(e.target.value)} /><br />
+          <input placeholder="Email" type='text' id='campo_email' value={email} onChange={(e) => setEmail(e.target.value)} /><br />
+          <input placeholder="Senha" type='password' id='campo_senha' value={senha} onChange={(e) => setSenha(e.target.value)} /><br />
+          <input placeholder="Repetir a Senha" type='password' id='campo_repete_senha' value={repeteSenha} onChange={(e) => setRepeteSenha(e.target.value)} /><br />
+          <input placeholder="Número de Telefone" type='text' id='campo_telefone' value={telefone} onChange={(e) => setTelefone(e.target.value)} /><br />
+          <input value="Cadastrar" type='submit' onClick={handleSubmit} /><br />
         </forms>
       </main>
     </>
